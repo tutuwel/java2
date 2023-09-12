386fafaa7b1fad9d1c6d6b8fd1f60b2f22d47488
tutuwel <12111744@mail.sustech.edu.cn>
2023-09-12 17:22:53 +0800

    //test the commit

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // 定义两个整数变量
+        int num1 = 5;
+        int num2 = 7;
+        int min=num1-num2;
+        // 计算它们的和
+        int sum = num1 + num2;
+
+        // 输出结果
+        System.out.println("两个数字的和是：" + sum);
+    }
+}
